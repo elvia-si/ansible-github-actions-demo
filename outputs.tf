9c593c102fe3290734f6f221c35b7f0127b84f20
elvia-si <elvia.sicuro@cloudreach.com>
2021-12-30 18:12:34 +0100

    destroy infrasttructure

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,3 @@ output "ec2_instance_ip" {
     value = aws_instance.my_public_server.public_ip
 }
 
-# resource "local_file" "ansible_cfg" {
-#   content = templatefile("inventory.tmpl",
-#     {
-#       webserver = aws_instance.my_public_server.public_ip
-      
-#     }
-#   )
-#   filename = "inventory"
-# }
