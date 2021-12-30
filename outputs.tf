64de2e79b24446cb618d8ea09b291b370eb4f9b9
elvia-si <elvia.sicuro@cloudreach.com>
2021-12-30 16:10:32 +0100

    trying terraform local file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "ec2_instance_ip" {
     value = aws_instance.my_public_server.public_ip
 }
+
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/templates/hosts.tpl",
+    {
+      webserver = aws_instance.my_public_server.public_ip
+      
+    }
+  )
+  filename = "../ansible/inventory/hosts.cfg"
+}
